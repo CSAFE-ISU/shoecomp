3d95732ff20796a2ef1e324ee57787f8269b126f
Gautham <ahgamut@gmail.com>
2025-02-15 00:25:33 -0600

    skip downloading action bar plugin
    also delete updater JARs from zip

--- .github/scripts/collect-jars.sh
+++ .github/scripts/collect-jars.sh
@@ -15,9 +15,9 @@ echo "copying shoecomp jar..."
 cp ../../target/shoecomp-1.0-SNAPSHOT-shaded.jar ./shoecomp-1.0.jar
 
 # https://figshare.com/articles/dataset/Custom_toolbars_and_mini_applications_with_Action_Bar/3397603
-echo "downloading action_bar.jar..."
-JAR_URL="https://figshare.com/ndownloader/files/42230733"
+# echo "downloading action_bar.jar..."
+# JAR_URL="https://figshare.com/ndownloader/files/42230733"
 # ActionBar needs this specific folder location in order to load icons
-mkdir -p ActionBar/icons
-cd ActionBar/icons
-wget -qO action_bar.jar $JAR_URL
+# mkdir -p ActionBar/icons
+# cd ActionBar/icons
+# wget -qO action_bar.jar $JAR_URL
--- .github/scripts/pkg-macosx.sh
+++ .github/scripts/pkg-macosx.sh
@@ -5,6 +5,8 @@ FIJI_FILENAME="fiji-macosx.zip"
 
 wget https://downloads.imagej.net/fiji/latest/fiji-macosx.zip -qO "$FIJI_FILENAME"
 
+zip --delete "$FIJI_FILENAME" 'Fiji.app/jars/imagej-updater*'
+zip --delete "$FIJI_FILENAME" 'Fiji.app/jars/imagej-uploader*'
 zip --delete "$FIJI_FILENAME" 'Fiji.app/macros/*'
 zip --delete "$FIJI_FILENAME" 'Fiji.app/plugins/*'
 zip --delete "$FIJI_FILENAME" 'Fiji.app/scripts/*'
